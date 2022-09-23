108d189c0d82bd5c1809b6f3b45f6fa8bab1dbb4
David Bradway <david.bradway@gmail.com>
2022-09-23 16:14:50 -0400

    refactor into more functions

--- createflyers.py
+++ createflyers.py
@@ -1,24 +1,14 @@
 from wand.image import Image as WandImage
 from exif import Image as ExifImage
 import what3words
-from borb.pdf import Document
-from borb.pdf import Page
-from borb.pdf import SingleColumnLayout
-from borb.pdf import PageLayout
-from borb.pdf import Paragraph
-from borb.pdf import PDF
-from borb.pdf import Alignment
+from borb.pdf import (Document, Page, SingleColumnLayout, PageLayout,
+Paragraph, PDF, Alignment, ChunkOfText, InlineFlow, Barcode,
+BarcodeType, UnorderedList, HexColor)
 from borb.pdf import Image as BorbImage
-from borb.pdf import ChunkOfText
-from borb.pdf import InlineFlow
-from borb.pdf import Barcode, BarcodeType
-from borb.pdf import UnorderedList
-from borb.pdf import HexColor
 from borb.pdf.canvas.layout.emoji.emoji import Emojis
 from borb.pdf.canvas.layout.annotation.remote_go_to_annotation import (
     RemoteGoToAnnotation,
 )
-
 import fitz
 
 from decimal import Decimal
@@ -28,7 +18,6 @@ import os
 
 from names import *
 import secrets
-
 
 geocoder = what3words.Geocoder(secrets.W3W_API_KEY)
 
@@ -40,7 +29,30 @@ def decimal_coords(coords, ref):
     return decimal_degrees
 
 
-def make_flyer(JpgFile, name, w3w, OutFolder, OutFile):
+def convertToJpg(HeicFolder, filename, JpgFolder):
+    SourceFile = os.path.join(HeicFolder, filename)
+    newfilename = filename.replace(".HEIC", ".jpg")
+    newfilename = newfilename.replace(".heic", ".jpg")
+    TargetFile = os.path.join(JpgFolder, newfilename)
+    if not os.path.exists(TargetFile):
+        img = WandImage(filename=SourceFile)
+        img.format = "jpg"
+        img.save(filename=TargetFile)
+        img.close()
+    return TargetFile
+
+
+def getW3w(TargetFile):
+    with open(TargetFile, "rb") as src:
+        img = ExifImage(src)
+        lat = decimal_coords(img.gps_latitude, img.gps_latitude_ref)
+        lon = decimal_coords(img.gps_longitude, img.gps_longitude_ref)
+        res = geocoder.convert_to_3wa(what3words.Coordinates(lat, lon))
+        w3w = f"{res['words']}"
+    return w3w
+
+
+def makeFlyer(JpgFile, name, w3w, OutFolder, OutFile):
     # create Document
     doc: Document = Document()
 
@@ -135,13 +147,14 @@ def make_flyer(JpgFile, name, w3w, OutFolder, OutFile):
     with open(os.path.join(OutFolder, OutFile + ".pdf"), "wb") as pdf_file_handle:
         PDF.dumps(pdf_file_handle, doc)
 
+
+def convertToPng(OutFolder, OutFile):
     # convert to PNG
     with fitz.open(os.path.join(OutFolder, OutFile + ".pdf")) as pdf:
         for page in pdf:
             zoom = 2  # zoom factor
             pix = page.get_pixmap(matrix=fitz.Matrix(zoom, zoom))
             pix.save(os.path.join(OutFolder, OutFile + ".png"))
-
 
 def main():
 
@@ -150,31 +163,17 @@ def main():
     OutFolder = "OutFolder"
 
     i = 0
-    print(f'"JpgFileName", "OutputName", "What3Words"')
+    print(f'"JpgPath", "What3Words", "OutputName"')
     for filename in os.listdir(HeicFolder):
+        # for each Heic file in the folder
         if filename.endswith(".heic") or filename.endswith(".HEIC"):
-            SourceFile = os.path.join(HeicFolder, filename)
-            newfilename = filename.replace(".HEIC", ".jpg")
-            newfilename = newfilename.replace(".heic", ".jpg")
-            TargetFile = os.path.join(JpgFolder, newfilename)
-            if not os.path.exists(TargetFile):
-                img = WandImage(filename=SourceFile)
-                img.format = "jpg"
-                img.save(filename=TargetFile)
-                img.close()
-
-            with open(TargetFile, "rb") as src:
-                img = ExifImage(src)
-                lat = decimal_coords(img.gps_latitude, img.gps_latitude_ref)
-                lon = decimal_coords(img.gps_longitude, img.gps_longitude_ref)
-                res = geocoder.convert_to_3wa(what3words.Coordinates(lat, lon))
-                w3w = f"{res['words']}"
-
+            TargetFile = convertToJpg(HeicFolder, filename, JpgFolder)
+            w3w = getW3w(TargetFile)
             name = names[i]
             OutFile = f"{i:02}-{name}"
-
-            print(f'"{newfilename}", "{OutFile}", "{w3w}"')
-            make_flyer(TargetFile, name, w3w, OutFolder, OutFile)
+            print(f'"{TargetFile}", "{w3w}", "{OutFile}"')
+            makeFlyer(TargetFile, name, w3w, OutFolder, OutFile)
+            convertToPng(OutFolder, OutFile)
             i = i + 1
 
 
